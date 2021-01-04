08dad1f0b457923d38b86c91b687770a92b82ae7
eduabundiz <3duabundiz@gmail.com>
2021-01-04 16:18:40 -0600

    Proptypes

---
+++ src/components/CarouselItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/static/icons/play-icon.png';
+import plusIcon from '../assets/static/icons/plus-icon.png';
+const CarouselItem = ({cover,title,year,contentRating, duration}) => (
+    <div className="carousel-item">
+        <img src={cover} className="carousel-item__img" alt={title}/>
+        <div className="carousel-item__details">
+            <div>
+                <img className="carousel-item__details--icon" src={playIcon} alt="Play"/>
+                <img className="carousel-item__details--icon" src={plusIcon} alt="Agregar"/>
+            </div>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">
+                {`${year} ${contentRating} ${duration}`}
+            </p>
+        </div>
+    </div>
+);
+
+CoarouselItem.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number,
+}
+ 
+export default CarouselItem;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-    </div>
-);
+const API = 'http://localhost:3000/initalState';
+
+const App = () => {
+
+    const initalState = useInitialState(API)    
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            {
+                initalState.mylist?.length > 0 && 
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        { initialState.milist?.map( item =>
+                            <CarouselItem key = {item.id} {...item} />
+                        )}                        
+                    </Carousel>
+                </Categories>
+            }
+            
+
+            <Categories title="Tendencias">
+                <Carousel>
+                    {
+                        initalState.trends?.map(item => 
+                            <CarouselItem key={item.id} {...item}/>
+                        )
+                    }
+
+                </Carousel>
+            </Categories>
+
+            <Categories title ="Originales de Platzi Video">
+                <Carousel>
+                    {
+                        initalState.originals?.map(item=>
+                                <CarouselItem key={item.id} {...item} />
+                            )
+                    }
+
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
